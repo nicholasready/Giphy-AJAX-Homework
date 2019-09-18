b984b1b1e30eb885c7d30f3016fb8209681bc4e8
Nicholas Ready <Ready.nicholas@gmail.com>
2019-09-17 19:06:54 -0500

    Fixed issue with Jquery link being in the wrong position. Buttons are now being displayed

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,1 +1,19 @@
-alert("hi!")
+var topics = ["Ariel", "Moana", "Cinderella", "Jasmine"];
+var newTopic = "";
+
+//Function to show all the buttons
+function showButtons() {
+    $("#buttonsHere").empty();
+    $("#princessInput").val("");
+
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<input type='button'/>");
+        button.addClass("princess");
+        button.attr("princess-name", topics[i]);
+        button.text(topics[i]);
+        $("#buttonsHere").append(button);
+        $("#buttonsHere").append(" ");
+    }
+}
+showButtons();
+
