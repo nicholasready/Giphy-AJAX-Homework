42df08ded951cdc71d97eb314e10f13b662dc25c
Nicholas Ready <Ready.nicholas@gmail.com>
2019-09-17 19:55:50 -0500

    Added the feature that takes the value in the input field and shows it as a new button

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -7,13 +7,29 @@ function showButtons() {
     $("#princessInput").val("");
 
     for (var i = 0; i < topics.length; i++) {
-        var button = $("<input type='button'/>");
-        button.addClass("princess");
-        button.attr("princess-name", topics[i]);
+        var button = $("<button>");
+        button.addClass("princess-btn");
+        button.attr("data-name", topics[i]);
         button.text(topics[i]);
         $("#buttonsHere").append(button);
         $("#buttonsHere").append(" ");
     }
 }
 showButtons();
+
+ 
+ $("#addPrincess").on("click", function(event) {
+    event.preventDefault();
+  
+    var princess = $("#princessInput").val().trim();
+
+    // Adding movie from the textbox to our array
+    topics.push(princess);
+
+    // Calling renderButtons which handles the processing of our movie array
+    showButtons();
+  });
+
+  // Adding a click event listener to all elements with a class of "movie-btn"
+  $(document).on("click", ".princess-btn", displayPrincessGifs);
 
